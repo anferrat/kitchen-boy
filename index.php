f93fae1d9af77ba8792a023bb2add7a5e70a81f8
dzou <dzou@company.com>
2018-08-22 18:58:33 -0600

    new

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 $verify_token = "kitchen";
 $token = "EAAbf6i0r8GoBAAoL1EVtXv1DibWI5lRfMU7r2YkGe3w5a3FnE73f0zxkhFY3mJiE6ACuwyD9IweseZCteAZB7J10PTJXRndTtzyhsV9wLUnwDtIkc2wGfjIoxof5n379YNEgP7le8yXPbtb5sqZAcWEqcJXZBIPRhWZClnTZAMZCuZAAuNQhtwGF";
-$hub_verify_token = null;
-if(isset($_REQUEST['hub_challenge'])) {
- $challenge = $_REQUEST['hub_challenge'];
- $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === $verify_token) {
-}
 
 use pimax\FbBotApp;
 use pimax\Messages\Message;

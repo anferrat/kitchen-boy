601f3e110cf8bdf5bd2827726fc83d73d07c9ce7
dzou <dzou@company.com>
2018-08-23 16:36:22 -0600

    new

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
 include 'fbbot.php';
 //$oo = nex_date('2170490766313202');
-//$bot->send(new Message('2170490766313202', $oo));
+$bot->send(new Message('2170490766313202', 'jhjhjh'));
 note_gen();
 
 ?>

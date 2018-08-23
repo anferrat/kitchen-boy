4dc89daf6b0845a8e6c406155910deef8bfb066d
dzou <dzou@company.com>
2018-08-22 20:16:40 -0600

    new

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'verify_token' => 'kitchen',
+    'token' => 'EAAbf6i0r8GoBAAoL1EVtXv1DibWI5lRfMU7r2YkGe3w5a3FnE73f0zxkhFY3mJiE6ACuwyD9IweseZCteAZB7J10PTJXRndTtzyhsV9wLUnwDtIkc2wGfjIoxof5n379YNEgP7le8yXPbtb5sqZAcWEqcJXZBIPRhWZClnTZAMZCuZAAuNQhtwGF'
+];
--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 $verify_token = "kitchen";
 $token = "EAAbf6i0r8GoBAAoL1EVtXv1DibWI5lRfMU7r2YkGe3w5a3FnE73f0zxkhFY3mJiE6ACuwyD9IweseZCteAZB7J10PTJXRndTtzyhsV9wLUnwDtIkc2wGfjIoxof5n379YNEgP7le8yXPbtb5sqZAcWEqcJXZBIPRhWZClnTZAMZCuZAAuNQhtwGF";
 
+if (file_exists(__DIR__.'/config.php')) {
+    $config = include __DIR__.'/config.php';
+    $verify_token = $config['verify_token'];
+    $token = $config['token'];
+}
+
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
 use pimax\FbBotApp;
@@ -26,15 +32,15 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
      $data = json_decode(file_get_contents("php://input"), true);
      if (!empty($data['entry'][0]['messaging']))
      {
-          //  foreach ($data['entry'][0]['messaging'] as $message)
-          //  {
+            foreach ($data['entry'][0]['messaging'] as $message)
+           {
 $content = file_get_contents("php://input");
 $fp = fopen("myText.txt","wb");
 fwrite($fp,$content);
 fclose($fp);
-//$bott->send(new Message($data['entry'][0]['messaging'][0]['sender']['id'], 'Hi there!'));
+$bott->send(new Message($message['sender']['id'], 'Hi there!'));
 
-        //    }
+            }
    }
 }
 

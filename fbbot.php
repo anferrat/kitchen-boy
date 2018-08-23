5fe92411e9568c0b7518eacacc2972f2c881b746
dzou <dzou@company.com>
2018-08-23 16:29:16 -0600

    new

--- fbbot.php
+++ fbbot.php
@@ -99,7 +99,7 @@ function reg_conf($idd2)
 {
     global $bot;
 	$dd = nex_date($idd2);
-	$bot->send(new Message($idd2, 'Your login request has been approved. You will recieve reminders when its your day to clean. Your next duty day is scheduled for'.$dd.'. You will recieve reminder in that day'));
+	$bot->send(new Message($idd2, 'Your login request has been approved. You will recieve reminders when its your day to clean. Your next duty day is scheduled for '.$dd.'. You will recieve reminder in that day'));
 
 	}
 
--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 
 include 'fbbot.php';
 $oo = nex_date('2170490766313202');
-$bot->send(new Message('2170490766313202',$oo));
+$bot->send(new Message('2170490766313202', $oo));
 //note_gen();
 
 ?>

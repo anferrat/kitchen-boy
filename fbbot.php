1197100b0aeb33ce57b685de4eb7522945e72d15
dzou <dzou@company.com>
2018-09-13 20:05:25 -0600

    new

--- fbbot.php
+++ fbbot.php
@@ -544,7 +544,7 @@ if (mysqli_query($conn, $sql2)) {
 if ($loc =='b')
 {
 	$new_order_wb = count($wash_b_names)+1;
-	$sql2 = "INSERT INTO ".$database.".washroom_basement (id, name, order) VALUES ('".$ms_id."', '".$new_name."', ".$new_order_wb.")";
+	$sql2 = "INSERT INTO ".$database.".washroom_basement (id, name, order) VALUES ('".$ms_id."', '".$new_name."', '".$new_order_wb."')";
 if (mysqli_query($conn, $sql2)) {
     $stat=1;
 } else {
@@ -554,7 +554,7 @@ if (mysqli_query($conn, $sql2)) {
 else if ($loc =='b')
 {
 	$new_order_wu = count($wash_u_names)+1;
-	$sql2 = "INSERT INTO ".$database.".washroom_upstairs (id, name, order) VALUES ('".$ms_id."', '".$new_name."', ".$new_order_wu.")";
+	$sql2 = "INSERT INTO ".$database.".washroom_upstairs (id, name, order) VALUES ('".$ms_id."', '".$new_name."', '".$new_order_wu."')";
 if (mysqli_query($conn, $sql2)) {
     $stat=1;
 } else {

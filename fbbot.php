78f10dd16670d0bc7886879710becb72e39b9dca
dzou <dzou@company.com>
2018-09-16 00:18:32 -0600

    new

--- fbbot.php
+++ fbbot.php
@@ -1036,31 +1036,6 @@ mysqli_close ($conn);
 	return $sch_w;
 }
 
-	global $conn;
-	
-$sql = "SELECT * FROM ".$database.".".$table;
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-	$o=1;
-    while($row = $result->fetch_assoc()) {
-        $h_name[$o] = $row["name"];
-		$o++;
-    }
-} 	
-for ($i=count($h_name);$i>1;$i--)
-{
-	$h_name[$i] = $h_name[$i-1];
-}
-$h_name[1] = $name;
-
-
-for ($i=1;$i<=count($h_name);$i++)
-{
-	$sql = "UPDATE ".$database.".".$table." SET `name` = '".$h_name[$i]."' WHERE id = ".$i;
-	mysqli_query($conn, $sql);	
-}
-
 }
 mysqli_close ($conn);
 ?>

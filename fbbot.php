add969ece0821389b003e5e16942aa08ef7e066a
dzou <dzou@company.com>
2018-09-16 01:21:15 -0600

    new

--- fbbot.php
+++ fbbot.php
@@ -1079,7 +1079,7 @@ function GetImageFromUrl($link) {
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
-	$result=curl_exec($ch) or die(curl_error($ch));
+	$result=curl_exec($ch);
 	curl_close($ch);
 	return $result;
 }
